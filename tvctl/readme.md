e2f9693423ebe66604cbba1439ab2a591fb78374
Balazs Kezes <3j4b0y1i@anonaddy.me>
2023-08-09 08:16:57 +0200

    tvctl: implement tvshow playing right in the server

--- tvctl/readme.md
+++ tvctl/readme.md
@@ -1,4 +1,18 @@
 # tvctl
 
 a silly service that starts automatically on my tv
-and can start a certain command if certain challenges are met.
+and can start play tv shows if certain challenges are met.
+
+the tv shows must be structured into directories.
+the name of the directory is the pass-phrase for starting that show.
+the server looks for the next episode to play in a subdirectory in the show's directory.
+to mark a show as "seen", it moves the episode to be directly in the show's directory.
+
+suppose you have the following shows:
+
+  tvshows/kidcartoon/next/s01e03.mkv
+  tvshows/kidcartoon/next/s01e04.mkv
+  tvshows/kidcartoon/s01e01.mkv
+  tvshows/kidcartoon/s01e02.mkv
+
+the next episode it will play will be s01e03.mkv after which it will move the episode one directory higher.
--- tvctl/serve.go
+++ tvctl/serve.go
@@ -1,37 +1,100 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 var (
-	port       = flag.Int("port", 21359, "the port for the server.")
-	rootPath   = flag.String("root", "", "if set, serve files from this dir.")
-	rewardPath = flag.String("reward", "./reward", "path to the reward script.")
+	port      = flag.Int("port", 21359, "the port for the server.")
+	rootPath  = flag.String("root", "", "if set, serve files from this dir.")
+	showsPath = flag.String("shows", "", "directory where the shows live.")
 )
+
+func respond(w http.ResponseWriter, code int, format string, v ...any) {
+	log.Printf(format, v...)
+	w.WriteHeader(code)
+	fmt.Fprintf(w, format, v...)
+}
 
 func rewardHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
 		return
 	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// find the next episode for the show in the body.
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		respond(w, http.StatusUnprocessableEntity, "read body: %v", err)
+		return
+	}
+	show := strings.ToLower(string(body))
+	if show == "" {
+		respond(w, http.StatusBadRequest, "missing request body")
+		return
+	}
+	log.Printf("received request for %q", show)
+	showdir := filepath.Join(*showsPath, show)
+	glob := filepath.Join(showdir, "*/*")
+	episodes, err := filepath.Glob(glob)
+	if err != nil {
+		respond(w, http.StatusInternalServerError, "glob: %v", err)
+		return
+	}
+	if len(episodes) == 0 {
+		respond(w, http.StatusInternalServerError, "glob %s: no episodes found", glob)
+		return
+	}
+	sort.Strings(episodes)
+	episode := episodes[0]
+
+	// mark the file as seen.
+	dst := filepath.Join(showdir, filepath.Base(episode))
+	err = os.Rename(episode, dst)
+	if err != nil {
+		respond(w, http.StatusInternalServerError, "%v", err)
+		return
+	}
+	respond(w, http.StatusOK, "ok")
+
+	// play the file in the background.
+	// todo: read mpv customization.
 	go func() {
-		cmd := exec.Command(*rewardPath)
+		cmd := exec.Command("mpv", dst)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		log.Print("got a reward request.")
-		log.Print("reward completed: ", cmd.Run())
+		log.Printf("running mpv %q.", dst)
+		log.Print("mpv completed: ", cmd.Run())
 	}()
 }
 
-func main() {
+func run() error {
+	logfile, err := os.OpenFile(filepath.Join(os.Getenv("HOME"), ".tvctl.log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		return fmt.Errorf("open logfile: %v", err)
+	}
+	log.SetOutput(io.MultiWriter(logfile, os.Stderr))
+
 	flag.Parse()
-	if len(*rootPath) > 0 {
+
+	if *showsPath == "" {
+		return errors.New("missing --shows")
+	}
+
+	if *rootPath == "" {
+		log.Print("warn: missing --root, won't serve fs.")
+	}
+	if *rootPath != "" {
 		fs := http.FileServer(http.Dir(*rootPath))
 		http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 			if req.URL.Path == "/" {
@@ -43,5 +106,13 @@ func main() {
 		})
 	}
 	http.HandleFunc("/reward", rewardHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+
+	log.Printf("serving at :%d", *port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
